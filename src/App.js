54f52a90e53a46f54c8b9875e3b9ec63b31630e4
Ajinkya Jadhav <ajaj51572@gmail.com>
2022-12-31 08:13:54 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
+import logo from './img.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+     const[addtext,setaddtext]=useState('');
+    const [data,setData]=useState([])
+console.log(data);
+
+        function  TextAdd(){
+            if(!addtext){
+           alert('plase fill up data');
+            }else{
+            setData( [...data,addtext]);
+            setaddtext('');
+        }
+
+        }
+
+function deleteText(id){
+ 
+    const deleteitem =data.filter((val,index)=>{
+       
+        return index!=id;
+});
+setData(deleteitem);
 }
 
+
+function remove(){
+setData([]);
+}
+
+    return(
+        <div>
+        <div className="main-container">
+        
+        <div className="img-logo">
+        <img src={logo}/>
+        </div>
+        
+<input type='text' value={addtext} onChange={(e)=>setaddtext(e.target.value)} placeholder='add a item' />
+<button onClick={TextAdd}>➕</button>
+
+{  data.map((elm,ind)=>
+            <div key={elm.id}  className="map-text">
+         <h3>{elm} </h3> 
+         <div className="edit-button">
+         
+         <button onClick={()=>deleteText(ind)}>❌</button>
+         </div>
+         
+       
+         </div>   
+         )
+        }
+            
+        <button onClick={remove}>reset all</button>
+        
+        </div>
+        </div>
+
+    )
+}
 export default App;
